c1fc94f9330b4ebfb98e6ed312a9d769b61f5104
Duong2k1 <19110009@student.hcmute.edu.vn>
2022-12-16 11:11:33 +0700

    Update UI Form Register

--- frontend-kltn/src/App.js
+++ frontend-kltn/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePageClient from './pages/client/HomePageClient';
 import LoginPageClient from './pages/client/LoginPageClient';
+import RegisterClient from './components/client/RegisterClient';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePageClient />} />
           <Route path='/login' element={<LoginPageClient />} />
+          <Route path='/register' element={<RegisterClient />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend-kltn/src/components/client/RegisterClient.jsx
+++ frontend-kltn/src/components/client/RegisterClient.jsx
@@ -14,11 +14,16 @@ const RegisterClient = () => {
     // Check validation form
     const formik = useFormik({
         initialValues: {
+            username: "",
             email: "",
             password: ""
         },
 
         validationSchema: Yup.object({
+            username: Yup.string()
+                .min(2, "Tên đăng nhập phải nhiều hơn 2 ký tự")
+                .max(15, "Tên đăng nhập không vượt quá 15 ký tự")
+                .required("Vui lòng cung cấp tên đăng nhập"),
             email: Yup.string()
                 .email("Định dạng email chưa chính xác")
                 .required("*Vui lòng cung cấp email"),
@@ -35,11 +40,21 @@ const RegisterClient = () => {
         <Row style={{ width: '100vw', height: '100vh', backgroundImage: 'linear-gradient(#39b0e5, #3aabd9, #b5dbe8)' }} className='mx-0 p-2 d-flex justify-content-center align-items-center'>
             <Col className='px-5 py-4 d-flex flex-column justify-content-between align-items-center' style={{ height: '90vh', background: '#f3f3f3', borderRadius: '10px' }} sm={10} md={8} lg={4} xl={4}>
                 <div className='d-flex flex-column justify-content-between align-items-center' style={{ width: '100%' }}>
-                    <Link to='/'>
-                        <Image style={{ height: '50px', width: '50px' }} src='https://res.cloudinary.com/dkzuyi8fq/image/upload/v1670986236/samples/KLTN/logo-Xstore_yqzhts.png' alt='logo'></Image>
-                    </Link>
-                    <h5 className='pt-4'>Đăng nhập</h5>
+                    <h4 className='py-2'>Đăng ký</h4>
                     <Form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Tên đăng nhập</Form.Label>
+                            <Form.Control
+                                type="text"
+                                name='username'
+                                placeholder="Nhập tên đăng nhập"
+                                value={formik.values.username}
+                                onChange={formik.handleChange} />
+                            {formik.errors.username && formik.touched.username && (
+                                <p style={{ color: 'red' }}>{formik.errors.username}</p>
+                            )}
+                        </Form.Group>
+
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
@@ -66,12 +81,11 @@ const RegisterClient = () => {
                             )}
                         </Form.Group>
                         <Form.Group className="mb-3 d-flex justify-content-between" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Hiển thị mật khẩu" onClick={togglePasswordVisiblity}/>
-                            <Link style={{ textDecoration: 'none', color: '#39b0e5' }} to='/'>Quên mật khẩu?</Link>
+                            <Form.Check type="checkbox" label="Hiển thị mật khẩu" onClick={togglePasswordVisiblity} />
                         </Form.Group>
                         <Form.Group className='d-flex justify-content-center'>
                             <Button style={{ background: '#39b0e5', border: 'none', margin: '0 auto', width: '100%' }} type="submit">
-                                Đăng nhập
+                                Đăng ký
                             </Button>
                         </Form.Group>
                         <Form.Group className='d-flex justify-content-center'>
@@ -81,15 +95,15 @@ const RegisterClient = () => {
                             <Button className='d-flex justify-content-center align-items-center' style={{ background: '#f3f3f3', border: '1px solid #bbbbbb', margin: '0 auto', width: '100%' }} type="submit">
                                 <Image className='me-3' style={{ width: '25px' }} src='https://res.cloudinary.com/dkzuyi8fq/image/upload/v1671161911/samples/KLTN/7465f30319191e2729668875e7a557f2_lf3cpy.png' alt='logo-Google'></Image>
                                 <p className='my-0 py-2' style={{ color: '#333333' }}>
-                                    Đăng nhập với Google
+                                    Đăng ký với Google
                                 </p>
                             </Button>
                         </Form.Group>
                     </Form>
                 </div>
                 <div>
-                    <p>Bạn chưa có tài khoản?
-                        <Link style={{ textDecoration: 'none', color: '#39b0e5' }} to='/register'> Đăng ký</Link>
+                    <p>Bạn đã có tài khoản?
+                        <Link style={{ textDecoration: 'none', color: '#39b0e5' }} to='/login'> Đăng nhập</Link>
                     </p>
                 </div>
             </Col>
