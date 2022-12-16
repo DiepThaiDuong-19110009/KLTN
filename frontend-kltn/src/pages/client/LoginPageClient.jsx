d088c374d45eda2860b51333a68b7f5f7669ee84
Duong2k1 <19110009@student.hcmute.edu.vn>
2022-12-16 10:58:16 +0700

    Update UI Form Login

---
+++ frontend-kltn/src/pages/client/LoginPageClient.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginClient from '../../components/client/LoginClient'
+
+const LoginPageClient = () => {
+    return (
+        <>
+            <LoginClient />
+        </>
+    )
+}
+
+export default LoginPageClient
