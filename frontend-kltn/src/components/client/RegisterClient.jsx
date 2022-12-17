bc7a72489a30fa4230c71ae71278cc28a484c978
Duong2k1 <19110009@student.hcmute.edu.vn>
2022-12-17 09:20:08 +0700

    Update UI Register

--- frontend-kltn/src/components/client/RegisterClient.jsx
+++ frontend-kltn/src/components/client/RegisterClient.jsx
@@ -21,23 +21,23 @@ const RegisterClient = () => {
 
         validationSchema: Yup.object({
             username: Yup.string()
-                .min(2, "Tên đăng nhập phải nhiều hơn 2 ký tự")
-                .max(15, "Tên đăng nhập không vượt quá 15 ký tự")
-                .required("Vui lòng cung cấp tên đăng nhập"),
+                .min(2, "*Tên đăng nhập phải nhiều hơn 2 ký tự")
+                .max(15, "*Tên đăng nhập không vượt quá 15 ký tự")
+                .required("*Vui lòng cung cấp tên đăng nhập"),
             email: Yup.string()
-                .email("Định dạng email chưa chính xác")
+                .email("*Định dạng email chưa chính xác")
                 .required("*Vui lòng cung cấp email"),
             password: Yup.string()
-                .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
-                .max(12, "Mật khẩu không vượt quá 12 ký tự")
+                .min(8, "*Mật khẩu phải có ít nhất 8 ký tự")
+                .max(12, "*Mật khẩu không vượt quá 12 ký tự")
                 .matches(
-                    /^(?=.*[a-z])/, "Mật khẩu phải có ít nhất 1 ký tự viết thường")
+                    /^(?=.*[a-z])/, "*Mật khẩu phải có ít nhất 1 ký tự viết thường")
                 .matches(
-                    /^(?=.*[A-Z])/, "Mật khẩu phải có ít nhất 1 ký tự viết hoa")
+                    /^(?=.*[A-Z])/, "*Mật khẩu phải có ít nhất 1 ký tự viết hoa")
                 .matches(
-                    /^(?=.*[0-9])/, "Mật khẩu phải có ít nhất 1 ký tự số")
+                    /^(?=.*[0-9])/, "*Mật khẩu phải có ít nhất 1 ký tự số")
                 .matches(
-                    /^(?=.*[!@#\$%\^&\*])/, "Mật khẩu phải có ít nhất 1 ký tự đặc biệt [!@#\$%\^&\*]")
+                    /^(?=.*[!@#$%^&*])/, "*Mật khẩu phải có ít nhất 1 ký tự đặc biệt !@#$%^&*")
                 .required("*Vui lòng cung cấp mật khẩu")
         }),
         onSubmit: values => {
@@ -47,11 +47,11 @@ const RegisterClient = () => {
 
     return (
         <Row style={{ width: '100vw', height: '100vh', backgroundImage: 'linear-gradient(#39b0e5, #3aabd9, #b5dbe8)' }} className='mx-0 p-2 d-flex justify-content-center align-items-center'>
-            <Col className='px-5 py-4 d-flex flex-column justify-content-between align-items-center' style={{ height: '90vh', background: '#f3f3f3', borderRadius: '10px' }} sm={10} md={8} lg={4} xl={4}>
+            <Col className='px-5 py-4 d-flex flex-column justify-content-between align-items-center' style={{ height: '95vh', background: '#f3f3f3', borderRadius: '10px' }} sm={10} md={8} lg={4} xl={4}>
                 <div className='d-flex flex-column justify-content-between align-items-center' style={{ width: '100%' }}>
                     <h4 className='py-2'>Đăng ký</h4>
                     <Form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Group className="mb-2" controlId="formBasicEmail">
                             <Form.Label>Tên đăng nhập</Form.Label>
                             <Form.Control
                                 type="text"
@@ -60,11 +60,11 @@ const RegisterClient = () => {
                                 value={formik.values.username}
                                 onChange={formik.handleChange} />
                             {formik.errors.username && formik.touched.username && (
-                                <p style={{ color: 'red' }}>{formik.errors.username}</p>
+                                <p className='mb-0' style={{ color: 'red', fontSize: '14px' }}>{formik.errors.username}</p>
                             )}
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Group className="mb-2" controlId="formBasicEmail">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 type="email"
@@ -73,11 +73,11 @@ const RegisterClient = () => {
                                 value={formik.values.email}
                                 onChange={formik.handleChange} />
                             {formik.errors.email && formik.touched.email && (
-                                <p style={{ color: 'red' }}>{formik.errors.email}</p>
+                                <p className='mb-0' style={{ color: 'red', fontSize: '14px' }}>{formik.errors.email}</p>
                             )}
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Group className="mb-2" controlId="formBasicPassword">
                             <Form.Label>Mật khẩu</Form.Label>
                             <Form.Control
                                 type={passwordShown ? "text" : "password"}
@@ -86,7 +86,7 @@ const RegisterClient = () => {
                                 value={formik.values.password}
                                 onChange={formik.handleChange} />
                             {formik.errors.password && formik.touched.password && (
-                                <p style={{ color: 'red' }}>{formik.errors.password}</p>
+                                <p className='mb-0' style={{ color: 'red', fontSize: '14px' }}>{formik.errors.password}</p>
                             )}
                         </Form.Group>
                         <Form.Group className="mb-3 d-flex justify-content-between" controlId="formBasicCheckbox">
@@ -114,6 +114,9 @@ const RegisterClient = () => {
                     <p>Bạn đã có tài khoản?
                         <Link style={{ textDecoration: 'none', color: '#39b0e5' }} to='/login'> Đăng nhập</Link>
                     </p>
+                </div>
+                <div>
+                    <Link style={{ textDecoration: 'none', color: '#39b0e5' }} to='/login'>Quay về trang chủ</Link>
                 </div>
             </Col>
         </Row>
